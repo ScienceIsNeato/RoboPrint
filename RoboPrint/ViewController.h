7232b61537d197ce2a990b513f148a9d5b22cc58
wmartin8 <wmartin8@utk.edu>
2014-04-05 18:04:58 -0600

    Initial Commit

---
+++ RoboPrint/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RoboPrint
+//
+//  Created by The Dude on 4/5/14.
+//  Copyright (c) 2014 William Martin Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RoboPrint/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoboPrint
+//
+//  Created by The Dude on 4/5/14.
+//  Copyright (c) 2014 William Martin Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
